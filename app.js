d7817baa92ec71c3d05b612350e852b304298d96
gaetanof <franciscogaetano1@gmail.com>
2023-04-29 19:30:35 +0200

    Creacion de proyecto de node.js y servidor de express con sus respectivos archivos y carpetas

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.resolve(__dirname,'./public')
+
+app.use(express.static('public'));
+
+app.get('/',(req, res) => {
+    let homeHTMLPath = path.join(__dirname,'./views/home.html')
+    res.sendFile(homeHTMLPath);
+});
+
+app.listen(3000,() => {console.log("Servidor escuchando puerto 3000")});
