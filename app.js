67f42dd61f24f70c924facdc274dd08fc9871dfa
gaetanof <franciscogaetano1@gmail.com>
2023-05-03 23:57:11 +0200

    actualizacion del archivo app.js para que se vean todas las pantallas cuando se corre el servior de express

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const publicPath = path.resolve(__dirname,'./public')
 app.use(express.static('public'));
 
 app.get('/',(req, res) => {
-    let homeHTMLPath = path.join(__dirname,'./views/login.html')
+    let homeHTMLPath = path.join(__dirname,'./views/home.html')
     res.sendFile(homeHTMLPath);
+});
+app.get('/login',(req, res) => {
+    let loginHTMLPath = path.join(__dirname,'./views/login.html')
+    res.sendFile(loginHTMLPath);
+});
+app.get('/signin',(req, res) => {
+    let signinHTMLPath = path.join(__dirname,'./signin/login.html')
+    res.sendFile(signinHTMLPath);
 });
 
 app.listen(3000,() => {console.log("Servidor escuchando puerto 3000")});
