50b03e16edad862a6949214e2f5c15c378e745c8
camilozv21 <camilozv21@gmail.com>
2023-05-15 20:42:50 -0500

    Avances MVC

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const usercontroller = require('../controllers/userController')
 const router = express.Router();
 
 router.get('/login', usercontroller.getLogin) // Se evita poner products
+router.get('/signin', usercontroller.getSigin) // Se evita poner products
 
 module.exports = router
