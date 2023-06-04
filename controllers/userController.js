006d20919a025a7e406086ef1e2c38efe57ab7f2
gaetanof <franciscogaetano1@gmail.com>
2023-06-04 20:20:28 -0300

    Unificacion de una parte de los archivos css y correccion de detalles

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,24 @@ const controllers = {
     },
     getSigin: (req, res) => {
         res.render('signin');
-    }
+    },
+    getUserList: (req, res) => {
+        let users = [
+            { id: 1, name: 'Juan Román Riquelme' },
+            { id: 2, name: 'Martín Palermo' },
+            { id: 3, name: 'Carlos Tevez' },
+        ];
+        
+        let usersResults = [];
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].name.includes(loQueBuscoElUsuario)) {
+                usersResults.push(users[i]);
+            }
+        }
+
+        res.render('userResults', { 'users': users });
+    },
+
 }
 
 module.exports = controllers;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.get('/login', usercontroller.getLogin) // Se evita poner products
 router.get('/signin', usercontroller.getSigin) // Se evita poner products
+router.get('/userResults', usercontroller.getUserList) // Se evita poner products
+router.get('/search', usercontroller.getUserList) // Se evita poner products
+
 
 module.exports = router
