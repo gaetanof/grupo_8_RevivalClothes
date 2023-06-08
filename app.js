64a735c7c818fb10a5bbee86f4ed92f5f3c9afaa
camilozv21 <camilozv21@gmail.com>
2023-06-08 14:57:10 -0500

    Finalización vista detalle-producto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override')
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
