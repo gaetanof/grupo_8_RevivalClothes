74c57bc91b0320dd4c0f2f8ce2b9f3a72193b378
camilozv21 <camilozv21@gmail.com>
2023-06-08 16:54:09 -0500

    Vista para añadir producto

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
-
+const multer = require('multer')
 const productController = require('../controllers/productController')
-
 const router = express.Router();
+
 
 router.get('/detalle', productController.getDetalle) // Se evita poner products
 router.get('/carrito-de-compras', productController.getCarrito);
