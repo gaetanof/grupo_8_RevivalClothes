c4804040ed629981faaf9181e7f0bb1a16de5c24
GatoMontess <agustinbravo27@gmail.com>
2023-06-14 20:44:27 -0300

    sube o no?

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,6 @@ router.get('/login', usercontroller.getLogin) // Se evita poner products
 router.get('/signin', usercontroller.getSigin) // Se evita poner products
 router.get('/userResults', usercontroller.getUserList) // Se evita poner products
 router.get('/search', usercontroller.getUserList) // Se evita poner products
-
+router.get('/edit:idUser', usercontroller.edit)
 
 module.exports = router
