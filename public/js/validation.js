73ff056e1618a56fbf04edcd8f1ffd37662b02ae
camilozv21 <camilozv21@gmail.com>
2023-08-18 16:40:08 -0500

    Validaciones del front

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,4 +1,4 @@
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
 
     //VALIDACIONES SIGNIN
     // validacion nombre
@@ -7,7 +7,7 @@ window.addEventListener("load",()=>{
     fullName.oninput = (e) => {
         const error = formularioSignIn.querySelector(".error-fullName")
         const input = e.target.value
-        if (input.length <= 2){
+        if (input.length <= 2) {
             error.classList.add("error-style")
             error.innerText = "El nombre debe tener al menos 3 caracteres"
         } else {
@@ -17,20 +17,75 @@ window.addEventListener("load",()=>{
     }
     // validacion email
     const email = formularioSignIn.querySelector(".formulario-signin-email")
-    const error = formularioSignIn.querySelector(".error-email")
 
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
     email.oninput = (e) => {
+        const error = formularioSignIn.querySelector(".error-email")
         const input = e.target.value
-        if (!emailPattern.test(input)){
+        if (!emailPattern.test(input)) {
             error.classList.add("error-style")
             error.innerText = "El email debe ser valido"
-        }else{
+        } else {
             error.classList.remove
             error.innerText = ""
         }
 
     }
+
+    // validacion username
+    const username = formularioSignIn.querySelector(".formulario-signin-username")
+
+    username.oninput = (e) => {
+        const input = e.target.value;
+        const error = formularioSignIn.querySelector(".error-username");
+        if (input.length <= 2) {
+            error.classList.add("error-style");
+            error.innerText = "El nombre debe tener al menos 3 caracteres"
+        } else {
+            error.classList.remove
+            error.innerText = ""
+        }
+    }
+
+    // validacion imagen
+    const imageInput = formularioSignIn.querySelector(".formulario-signin-image");
+    const imageError = formularioSignIn.querySelector(".error-image");
+
+    imageInput.onchange = (e) => {
+        const file = e.target.files[0];
+        console.log(e.target.files[0]);
+
+        if (!file) {
+            imageError.classList.add("error-style");
+            imageError.innerText = "Debes seleccionar una imagen";
+        } else {
+            const allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
+            const isImageValid = allowedExtensions.test(file.name);
+
+            if (!isImageValid) {
+                imageError.classList.add("error-style");
+                imageError.innerText = "Formato de imagen inválido (JPG, JPEG, PNG, GIF)";
+            } else {
+                imageError.classList.remove("error-style");
+                imageError.innerText = "";
+            }
+        }
+    }
+
+    // Validacion password 
+    const password = formularioSignIn.querySelector('.formulario-signin-password');
+    password.oninput = (e) => {
+        const error = formularioSignIn.querySelector(".error-password");
+        var patron = /^(?=.*[A-Z])(?=.*\d).+$/;
+
+        if (!patron.test(e.target.value)) {
+            error.innerText = "La contraseña debe contener al menos una letra mayúscula y al menos un número.";
+        } else {
+            error.classList.remove("error-style");
+            error.innerText = "";
+        }
+    }
+
 })
 
 
