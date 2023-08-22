0251f4b8b46405c912ff7b62a35ea893af7755e3
camilozv21 <camilozv21@gmail.com>
2023-08-22 16:46:43 -0500

    fixing apis

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -5,7 +5,38 @@ const controller = {
     getAll: async (req, res) => {
         const response = await Product.findAll({ raw: true })
 
+        let contDeportiva = 1;
+        let contSombrero = 1;
+        let contAdulto = 1;
+        let contMarca = 1;
+        let contNino = 1;
+
+        for (let i = 0; i < response.length; i++) {
+            if (response[i].category == 'adulto') {
+                contAdulto += 1;
+            } else if (response[i].category == 'ninos') {
+                contNino += 1;
+            } else if (response[i].category == 'deportiva') {
+                contDeportiva += 1;
+            } else if (response[i].category == 'marca') {
+                contMarca += 1;
+            } else if (response[i].category == 'sombreros') {
+                contSombrero += 1;
+            } else if (response[i] == undefined) {
+                continue;
+            }
+        }
+
+        const countByCategory = {
+            contDeportiva,
+            contSombrero,
+            contAdulto,
+            contMarca,
+            contNino
+        }
+
         return res.json({
+            countByCategory,
             total: response.length,
             data: response
         })
