f3e96e2542ec1ffa92adf06bb016102e2f7be831
camilozv21 <camilozv21@gmail.com>
2023-08-27 10:38:54 -0500

    inicio de react

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,11 @@
 import './assets/App.css';
 import Card from './components/Card';
 
+const product = fetch('http://localhost:5001/api/10/products')
+  .then(res => res.json())
+  .then(product => (product));
+
+console.log(product);
 
 function App() {
   return (
