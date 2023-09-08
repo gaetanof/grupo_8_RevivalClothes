73fd786e7c28dea5f8165fc2b32ad6e8653f7997
camilozv21 <camilozv21@gmail.com>
2023-09-08 11:11:17 -0500

    inicio de correcciones

--- Frontend/src/components/Category.js
+++ Frontend/src/components/Category.js
@@ -3,7 +3,7 @@ import React from "react";
 function Category(props) {
     return (
         <div className="col-lg-6 mb-4">
-            <div className="card bg-dark text-white shadow">
+            <div className="card bg-gradient-dark text-white shadow">
                 <div className="card-body">
 
                     <span>{props.texto}</span>
